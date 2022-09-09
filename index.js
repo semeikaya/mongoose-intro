7588e5fbf51c0290f10e01c7bb3fb8cdb656df53
semeikaya <106886023+semeikaya@users.noreply.github.com>
2022-09-09 15:23:43 +0300

    поставил роут перед подключением сервера

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const express = require("express");
 const app = express();
 app.use(express.json())
 
+app.use(require("./routes/students.route.js"));
+
 mongoose.connect("mongodb+srv://bersyak:web2000web@cluster0.akjwcf0.mongodb.net/test", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => console.log('Успешно соединились с сервером MongoDB'))
     .catch(() => console.log('Ошибка при соединении с сервером MongoDB'))
 
-app.use(require("./routes/students.route.js"));
+
 
 app.listen(4000, () => {
     console.log('Сервер успешно запущен');
